8a53f50917610ed73e70f5611488af5e66d723b6
RatulAhmed <ratulahmed1@gmail.com>
2019-06-07 14:27:57 -0400

    added campus changes and deleted default App data hello world, switching to seperate branch

---
+++ src/components/Campus.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+
+class Campus extends Component {
+	constructor {
+		super();
+
+		this.state = {
+			campusName: "",
+			campusAddress: "",
+			imageURL : "",
+			descript: "",
+			studentNames: []
+		}
+		this.blankState = this.state;
+	}
+
+	
+
+
+}
