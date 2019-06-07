99e27d418071a3f129d2b07ff03bd7809b1f2e73
RatulAhmed <ratulahmed1@gmail.com>
2019-06-07 15:19:36 -0400

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Campus from './components/Campus'
-
-class App extends Component() {
-	constructor{
-		super();
-	}
-
-  render() {
-  return (
-  	<div>
-  	<Campus/>
-  	</div>
-  	);
-	}
-=======
 import Nav from './components/nav.js'
 
 function App() {
@@ -25,7 +9,6 @@ function App() {
         <Nav/>
     </div>
   );
->>>>>>> 45f36494fb86e1cf8ebb845145f7d01aae84933f
 }
 
 export default App;
