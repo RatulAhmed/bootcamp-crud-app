6336d2c4306602e46e721511b06cc1a5e93ad25a
RatulAhmed <ratulahmed1@gmail.com>
2019-06-07 15:04:41 -0400

    deleting render

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -7,25 +7,16 @@ class Campus extends Component {
 		super();
 
 		this.state = {
-			campusName: "campus",
-			campusAddress: "123 Four Five Street",
-			imageURL : "/test.jpg",
-			description: "Testing Campus Component",
+			campusName: "",
+			campusAddress: "",
+			imageURL : "",
+			descript: "",
 			studentNames: []
 		}
 		this.blankState = this.state;
 	}
 
-	render() {
-		<div>
-		<ul>
-		<li>{this.state.campusName}</li>
-		<li>{this.state.campusAddress} </li>
-		<li>{this.state.imageURL} </li>
-		<li>{this.state.description} </li>
-		</ul>
-		</div>
-	}	
+	
 
 
 }
