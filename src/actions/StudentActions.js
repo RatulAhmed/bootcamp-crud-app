873251d175cee08765f5941d7419bf1b76f7d6f7
Karanveer Singh <karansingh98123@gmail.com>
2019-06-10 14:59:09 -0400

    added new variables to refer to the action types and exported them

--- src/actions/StudentActions.js
+++ src/actions/StudentActions.js
@@ -1,20 +1,24 @@
-export function editStudentInfo(modifiedStudent) {
+export const EDIT_STUDENT_INFO = "EDIT_STUDENT_INFO";
+export const REMOVE_STUDENT = "REMOVE_STUDENT";
+export const ADD_STUDENT = "ADD_STUDENT";
+
+export const editStudentInfo = (modifiedStudent) => {
     return {
-        type: "EDIT_STUDENT_INFO",
+        type: EDIT_STUDENT_INFO,
         payload: modifiedStudent
     }
 }
 
-export function removeStudent(studentID) {
+export const removeStudent = (studentID) => {
     return {
-        type: "REMOVE_STUDENT",
+        type: REMOVE_STUDENT,
         payload: studentID
     }
 }
 
-export function addStudent(newStudent) {
-    return{
-        type: "ADD_STUDENT",
+export const addStudent = (newStudent) => {
+    return {
+        type: ADD_STUDENT,
         payload: newStudent
     }
 }
