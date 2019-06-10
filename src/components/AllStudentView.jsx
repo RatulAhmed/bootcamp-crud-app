9e2d18b485d86e73162dd26fde09d857d06f943b
Karanveer Singh <karansingh98123@gmail.com>
2019-06-10 15:07:05 -0400

    renamed file to AllStudentView.js

---
+++ src/components/AllStudentView.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class AllStudentView extends Component {
+
+    constructor(props){
+        super(props);
+    };
+
+    render() {
+        return(
+            <div>
+                <h1> All Students </h1>
+
+
+        );
+    }
+
+
+};
+
+export default StudentView;
