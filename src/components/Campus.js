69ced64b6e22a9985cac4cefc22761ec469a30f9
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-12 14:24:20 -0400

    style campus

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
-
+import '../styles/allCampus.css'
 
 class Campus extends Component {
-	constructor {
+	constructor(){
 		super();
 
 		this.state = {
@@ -15,8 +15,32 @@ class Campus extends Component {
 		}
 		this.blankState = this.state;
 	}
+	render(){
 
-	
-
-
+		return (
+				<div className="campus-flex-container">
+					<div className="campus-box">
+							<div className="campus-flex-container">
+								 <div className="img-container">
+								 	<img src="./img/hunter.jpeg"/>
+								 </div>
+								 <div className="data-container">
+								 			<div className="campus-bar">
+								 	    	 <div className="campus-name"></div>
+											</div>
+											<div className="campus-bar">
+													<div className="bar-button"></div>
+								 					<div className="del-button"></div>
+											</div>
+											<div className="campus-bar">
+													<div className="bar-button"></div>
+													<div className="del-button"></div>
+											</div>
+								 </div>
+							 </div>
+					</div>
+				</div>
+		)
+	}
 }
+export default Campus
